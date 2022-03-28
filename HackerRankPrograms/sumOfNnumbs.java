c7131417618826f40074f7b04fb002838692b5de
Kartik-Vyas <101496656+Kartik-Vyas@users.noreply.github.com>
2022-03-28 22:12:05 +0530

    Create sumOfNnumbs.java
    Sample Input
    
    2
    0 2 10
    5 3 5
    Sample Output
    
    2 6 14 30 62 126 254 510 1022 2046
    8 14 26 50 98

---
+++ HackerRankPrograms/sumOfNnumbs.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+   //Math.pow(2,2)
+   Scanner sc = new Scanner(System.in);
+   int iteration = sc.nextInt();
+   for(int i=0;i<iteration; i++)
+   {
+       int a = sc.nextInt();
+       int b = sc.nextInt();
+       int c = sc.nextInt();
+       int value = ((int)(a+((Math.pow(2,0))*b)));
+       System.out.print(value + " ");
+       for(int j=1;j<c;j++)
+       {
+        value = ((int)(((Math.pow(2,j))*b)))+ value;
+          System.out.print(value +" ");
+       }
+       System.out.println();
+   }
+   
+    }
+}
