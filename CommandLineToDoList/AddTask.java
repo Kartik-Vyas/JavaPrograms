028bb2aa9a770b53b549d337797c134a65a497c2
Kartik-Vyas <101496656+Kartik-Vyas@users.noreply.github.com>
2022-06-06 22:02:34 +0530

    Create AddTask.java

---
+++ CommandLineToDoList/AddTask.java
@@ -0,0 +1,11 @@
+package com.second;
+
+import java.util.ArrayList;
+
+    class AddTask {
+    public static void adding(String s,ArrayList content)
+    {
+        content.add(s);
+        System.out.println("Your task added successfully");
+    }
+}
