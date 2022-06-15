51fcd505ae92eeefc4b19dc47de0e7544b9f9e37
Kartik-Vyas <101496656+Kartik-Vyas@users.noreply.github.com>
2022-06-15 22:26:09 +0530

    Create remove_spaces.java

---
+++ String/remove_spaces.java
@@ -0,0 +1,13 @@
+public class remove_spaces
+{
+	public static void main(String args[])
+	{
+		String str1 = " Hello World ";
+		System.out.println(str1);
+		System.out.println(str1.trim());
+
+		String str2 = "	 Hey there Joey!!!	 ";
+		System.out.println(str2);
+		System.out.println(str2.trim());
+	}
+}
